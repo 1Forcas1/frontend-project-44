50a1677d8a7273a5f15d082068d859058f9ce8fb
Danil Sichinava <danya.sichinava@mail.ru>
2022-12-08 14:35:32 +0300

    upgrade bin/brain-even.js and bin/brain-games.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,30 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import even from '../src/games/even.js';
 
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-for (let i = 0; i < 3; i += 1) {
-  const number = Math.ceil(Math.random() * 100);
-  console.log(`Question: ${number}`);
-  const userResponse = readlineSync.question('Your answer: ');
-
-  if ((number % 2 === 0 && userResponse === 'yes') || (number % 2 !== 0 && userResponse === 'no')) {
-    console.log('Correct!');
-  } else if (number % 2 === 0 && userResponse !== 'yes') {
-    console.log(`'${userResponse}' is wrong answer ;(. Correct answer was 'yes'.`);
-    console.log(`Let's try again, ${name}`);
-    break;
-  } else {
-    console.log(`'${userResponse}' is wrong answer ;(. Correct answer was 'no'.`);
-    console.log(`Let's try again, ${name}`);
-    break;
-  }
-
-  if (i === 2) {
-    console.log(`Congratulations, ${name}`);
-  }
-}
+even();
