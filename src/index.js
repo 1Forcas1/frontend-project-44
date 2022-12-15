7a2a97f8baac42c6503b82b61ca1d578c759f0fb
Danil Sichinava <danya.sichinava@mail.ru>
2022-12-15 23:02:42 +0300

    upgrade src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const basisOfGame = (taskDescription, task) => {
+const runBasisOfGameAndGameGeneration = (taskDescription, task) => {
   console.log('Welcome to the Brain Games!');
 
   const name = readlineSync.question('May I have your name? ');
@@ -8,24 +8,22 @@ const basisOfGame = (taskDescription, task) => {
 
   console.log(taskDescription);
 
-  for (let i = 0; i < 3; i += 1) {
-    const questionAndResult = task();
+  const numberOfRounds = 3;
+  for (let i = 0; i < numberOfRounds; i += 1) {
+    const [expression, correctAnswer] = task();
 
-    console.log(`Question: ${questionAndResult[0]}`);
+    console.log(`Question: ${expression}`);
     const userResponse = readlineSync.question('Your answer: ');
 
-    if (userResponse === questionAndResult[1]) {
+    if (userResponse === correctAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`'${userResponse}' is wrong answer ;(. Correct answer was '${questionAndResult[1]}'.`);
+      console.log(`'${userResponse}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
-      break;
-    }
-
-    if (i === 2) {
-      console.log(`Congratulations, ${name}!`);
+      return;
     }
   }
+  console.log(`Congratulations, ${name}!`);
 };
 
-export default basisOfGame;
+export default runBasisOfGameAndGameGeneration;
